e9e68459bf947bd0ad1dc58412ea2ab5d1f231ff
Jesco Wuester <jescowuester@gmail.com>
2021-04-08 00:37:20 +0200

    guess this is mvp?

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,24 @@
-<script>
-</script>
-
-<div class="flex flex-col h-screen">eee</div>
+<div class="w-full h-screen flex items-center justify-center bg-red-700">
+	<svg class="max-w-full max-h-full bg-white" viewBox="0 0 2000 1000" fill="none" xmlns="http://www.w3.org/2000/svg">
+		<g filter="url(#filter0_d)">
+			<rect x="324" y="67" width="1351" height="865" rx="10" fill="#181818" />
+		</g>
+		<circle cx="359.5" cy="98.5" r="8" stroke="white" />
+		<circle cx="388.5" cy="98.5" r="8" stroke="white" />
+		<circle cx="417.5" cy="98.5" r="8" stroke="white" />
+		<foreignObject x="324" y="128" width="1351" height="804">
+			<iframe id="inlineFrameExample" title="Inline Frame Example" class="w-full h-full" src="http://localhost:3000/" />
+		</foreignObject>
+		<defs>
+			<filter id="filter0_d" x="287" y="34" width="1425" height="939" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+				<feFlood flood-opacity="0" result="BackgroundImageFix" />
+				<feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" />
+				<feOffset dy="4" />
+				<feGaussianBlur stdDeviation="18.5" />
+				<feColorMatrix type="matrix" values="0 0 0 0 0.486052 0 0 0 0 0.471106 0 0 0 0 0.545833 0 0 0 0.25 0" />
+				<feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow" />
+				<feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow" result="shape" />
+			</filter>
+		</defs>
+	</svg>
+</div>
